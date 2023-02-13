0d133413bc9f76d99439ca8c25e0a4f804291f2f
Israel Solís Iglesias <72994140+IsraelSI11@users.noreply.github.com>
2023-02-13 19:00:55 +0100

    SDI-2223-312-2.1-Create Spring project.

---
+++ src/test/java/com/uniovi/sdi2223312spring/Sdi2223312SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223312spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223312SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
