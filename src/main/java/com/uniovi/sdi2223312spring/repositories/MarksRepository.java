ace04ae01b155d60e9245c2c72b5b2c27ee61175
Israel Solís Iglesias <72994140+IsraelSI11@users.noreply.github.com>
2023-02-14 15:57:00 +0100

    SDI-2223-312-2.4-Modelo, acceso a datos simple.

---
+++ src/main/java/com/uniovi/sdi2223312spring/repositories/MarksRepository.java
@@ -0,0 +1,7 @@
+package com.uniovi.sdi2223312spring.repositories;
+
+import com.uniovi.sdi2223312spring.entities.Mark;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarksRepository extends CrudRepository<Mark, Long> {
+}
