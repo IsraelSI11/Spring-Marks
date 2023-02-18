512c55afc8d9995e53f966bea642529bf1e372fd
Israel Solís Iglesias <72994140+IsraelSI11@users.noreply.github.com>
2023-02-18 13:04:54 +0100

    SDI-2223-312-3.5-Validación de datos en el servidor.

--- src/main/java/com/uniovi/sdi2223312spring/controllers/UsersController.java
+++ src/main/java/com/uniovi/sdi2223312spring/controllers/UsersController.java
@@ -3,11 +3,14 @@ package com.uniovi.sdi2223312spring.controllers;
 import com.uniovi.sdi2223312spring.entities.User;
 import com.uniovi.sdi2223312spring.services.SecurityService;
 import com.uniovi.sdi2223312spring.services.UsersService;
+import com.uniovi.sdi2223312spring.validators.SignUpFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,9 @@ public class UsersController {
 
     @Autowired
     private SecurityService securityService;
+
+    @Autowired
+    private SignUpFormValidator signUpFormValidator;
 
     @RequestMapping("/user/list")
     public String getListado(Model model) {
@@ -64,12 +70,23 @@ public class UsersController {
         return "redirect:/user/details/" + id;
     }
 
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user,result);
+        if(result.hasErrors()){
+            return "signup";
+        }
         usersService.addUser(user);
-        securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
+        securityService.autoLogin(user.getDni(),user.getPasswordConfirm());
         return "redirect:home";
     }
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
         return "login";
