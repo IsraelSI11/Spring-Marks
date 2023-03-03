08968ea3d7d2b5de122ac20cc5904b1e2ccdbae8
Israel Solís Iglesias <72994140+IsraelSI11@users.noreply.github.com>
2023-03-03 10:34:27 +0100

    SDI-2223-312-sesión4-Ejercicio-complementario3

--- src/main/java/com/uniovi/sdi2223312spring/CustomConfiguration.java
+++ src/main/java/com/uniovi/sdi2223312spring/CustomConfiguration.java
@@ -1,5 +1,6 @@
 package com.uniovi.sdi2223312spring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
@@ -16,6 +17,12 @@ import java.util.Locale;
 
 @Configuration
 public class CustomConfiguration implements WebMvcConfigurer {
+
+    @Value("${spring.data.web.pageable.default-page-size}")
+    private int size;
+
+    @Value("${spring.data.web.pageable.page-parameter}")
+    private int page;
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
@@ -36,8 +43,6 @@ public class CustomConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        int page = 0;
-        int size = 5;
         PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
         resolver.setFallbackPageable(PageRequest.of(page, size));
         argumentResolvers.add(resolver);
